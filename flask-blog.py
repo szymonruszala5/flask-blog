33f5a512d65bc0b9a9aa737aa18418c8b1f32724
szymon <szymonruszala5@gmail.com>
2024-04-04 20:21:00 +0200

    test

--- flask-blog.py
+++ flask-blog.py
@@ -1,6 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Szymon Ruszała",
+        "title": "Blog post 1",
+        "content": "First post content",
+        "date_posted": "April 20, 2021",
+    },
+    {
+        "author": "Szymon Ruszała",
+        "title": "Blog post 2",
+        "content": "First post content",
+        "date_posted": "April 20, 2021",
+    },
+    {
+        "author": "Szymon Ruszała",
+        "title": "Blog post 3",
+        "content": "First post content",
+        "date_posted": "April 20, 2021",
+    },
+]
+
 
 @app.route("/")
 def hello():
-    return "hello world 2" 
+    return render_template("home.html", posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
