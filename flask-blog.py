9de5f0e9c61035b8073b14783fac9e8b9f0f1efd
szymon <szymonruszala5@gmail.com>
2024-04-08 19:06:12 +0200

    changes

--- flask-blog.py
+++ flask-blog.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SECRET_KEY"] = "89599b61a651621815a28fa182a2e563"
 
 posts = [
     {
@@ -25,13 +29,33 @@ posts = [
 
 
 @app.route("/")
-def hello():
+def home():
     return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"Succesful login!", "success")
+        return redirect(url_for("home"))
+
+    return render_template("login.html", title="Login", form=form)
 
 
 if __name__ == "__main__":
